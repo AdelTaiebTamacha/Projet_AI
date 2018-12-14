b6165fc2f6eec62d453a7490085bb429bbbfea6e
lefeuvpi <45451927+lefeuvpi@users.noreply.github.com>
2018-12-14 14:19:52 +0100

    Petit dernier

--- type.ml
+++ type.ml
@@ -1,1 +1,1 @@
-type paire_portion = { ent_int : int*int; ent_ext : int*int; sort_int : int*int; sort_ext : int*int };;
+type portion = { ent_int : int*int; ent_ext : int*int; sort_int : int*int; sort_ext : int*int };;
