08aa074ba30c4a5428b03a9bed4568ad521d088e
lefeuvpi <45451927+lefeuvpi@users.noreply.github.com>
2018-12-14 13:20:40 +0100

    Modif type

--- type.ml
+++ type.ml
@@ -1,5 +1,8 @@
-type portion =
+type point =
   | EntInt of int*int
   | EntExt of int*int
   | SortInt of int*int
   | SortExt of int*int;;
+
+type trapeze =
+  | Portion of point*point*point*point;;
